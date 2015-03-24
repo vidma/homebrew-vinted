3a0e1cbef73346dac9fd520e2c6ecffc7cf7df1c
vidma <vidmantas.zemleris@gmail.com>
2015-03-24 14:35:30 +0200

    Upgrade Spark to 1.3.0 for CDH 5.3.2

--- cdh-spark.rb
+++ cdh-spark.rb
@@ -3,9 +3,9 @@ require "formula"
 class CdhSpark < Formula
   homepage "http://spark.apache.org/"
   head "https://github.com/cloudera/spark.git"
-  url "https://vinted-analytics.s3.amazonaws.com/spark-1.2.0-cdh5.3.0.tgz"
-  version "1.2.0-cdh5.3.0"
-  sha1 "43d93663625afcb48c6b171e23739a46bf84e809"
+  url "https://vinted-analytics.s3.amazonaws.com/spark-1.3.0-bin-spark-1.3.tgz"
+  version "1.3.0-cdh5.3.2"
+  sha1 "6d656c3e50d676b2b2b1afb71bbabaf58903ab3c"
 
   conflicts_with 'hive', :because => 'both install `beeline` binaries'
 
